b78362c59e581092e863600a02195e04f9d8e711
yahya0554135453-glitch <yahya0554135453@gmail.com>
2026-06-30 07:21:02 +0300

    Refactor language and modal handling in Y.js

--- Y.js
+++ Y.js
@@ -73,6 +73,21 @@ langBtn.addEventListener("click", () => {
 
 setLanguage("en");
 
+window.addEventListener("scroll", () => {
+
+    const navbar = document.querySelector(".navbar");
+
+    if(window.scrollY > 50){
+
+        navbar.classList.add("scrolled");
+
+    }else{
+
+        navbar.classList.remove("scrolled");
+
+    }
+
+});
 
 /* =========================
        1 UNIVERSITY MODAL
